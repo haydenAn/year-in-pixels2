71240363d2dbd373e0c0d068f4d4bdaa47e3cd94
Hayden Rahn <anhayzzing33@gmail.com>
2018-08-24 23:58:24 -0500

    quote react set up

--- src/components/Inbox/Quote/Quote.js
+++ src/components/Inbox/Quote/Quote.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import "./Quote.css"
+import {connect} from "react-redux"
+import {getQuote} from "../../../ducks/quote" 
 class Quote extends React.Component{
+    componentDidMount(){
+        this.props.getQuote()
+    }
     render(){
+        const {quote} = this.props;
         return(
-            <div className="Quote"></div>
+            <div className="Quote">
+            {
+                quote?
+                <div className="displayQuote">
+                <h1>{quote.text}</h1>
+                <p>{quote.author}</p>
+                </div>
+                :
+                <div>
+                    do you want to add a quote?
+                    </div>
+            }
+            </div>
         )
     }
 }
 
-
-export default Quote;
+const mapStateToProps= state=>{
+    return{
+        ...state.quote
+    }
+}
+export default connect(mapStateToProps,{getQuote})(Quote);
