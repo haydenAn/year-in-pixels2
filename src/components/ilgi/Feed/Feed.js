f21649358fab3c747ed624efabca6700ae96116a
Hayden Rahn <anhayzzing33@gmail.com>
2018-08-26 12:44:46 -0500

    feed resonsive

--- src/components/ilgi/Feed/Feed.js
+++ src/components/ilgi/Feed/Feed.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import {getFullPixels} from "../../../ducks/pixel"
 import {Card,CardContent,CardHeader,CardMedia,IconButton} from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import colors from "../Pixel/models/colors"
 class Feed extends React.Component {
     componentDidMount(){
         this.props.getFullPixels()
@@ -13,23 +13,28 @@ class Feed extends React.Component {
     console.log(this.props.pixels)
     const {pixels} = this.props,
     fullPixelDisplay=pixels.map((el,i)=>{
+      const moodObj = colors.filter((obj)=>el.colorvalue===Object.values(obj)[0])[0]
       return(
-         <Card key={i}>
+         <Card key={i} className="Feed_card">
            <CardHeader
-           style={{backgroundColor:el.colorvalue}}
+           className="Feed_header"
+           style={{backgroundColor:el.colorvalue, opacity:el.opacity}}
           action={
             <IconButton>
               <MoreVertIcon />
             </IconButton>
           }
-          title={el.colorvalue}
-          subheader={el.pixel_date}
+          title={Object.keys(moodObj)[0]}
+          subheader={el.colorvalue}
         />
          <CardMedia
          component="img"
           image={el.img_url}
         />
-        <CardContent>{el.text}</CardContent>
+        <CardContent className="Feed_content">
+        <h1>{el.pixel_date}</h1>
+        {"   "}{el.text}
+        </CardContent>
       </Card>
       )
      
